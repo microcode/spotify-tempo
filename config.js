8fdffeec86e27d4f2dcff2278870b187c990d52c
Jesper Svennevid <github@svennevid.se>
2015-04-03 09:36:41 +0200

    Added config

---
+++ config.js
@@ -0,0 +1,13 @@
+config = {
+    Templates: [
+        'root',
+        'header',
+        'spotify',
+        'echonest',
+        'playlist',
+        'playlist-item',
+        'tracks',
+        'tracks-item',
+        'player'
+    ]
+};
